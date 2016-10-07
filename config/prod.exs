22c9cd46530bb3f2c8a27db8a7713823a1c7a007
Federico Rebora <federico.rebora@carwow.co.uk>
2016-10-07 13:36:20 +0000

    Should have commented this line

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :rumbl, Rumbl.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "mysterious-meadow-6277.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
@@ -66,4 +67,4 @@ config :rumbl, Rumbl.Repo,
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
