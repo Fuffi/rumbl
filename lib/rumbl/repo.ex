aa646028aab6fba2e7ad691bb6a9d508effb1a79
Federico Rebora <federico.rebora@carwow.co.uk>
2016-10-07 12:00:44 +0000

    Using Ecto instead of stub

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,26 +1,3 @@
 defmodule Rumbl.Repo do
-  @moduledoc """
-  In memory repository.
-  """
-
-  def all(Rumbl.User) do
-    [
-      %Rumbl.User{id: "1", name: "Dante", username: "dante", password: "alighieri"},
-      %Rumbl.User{id: "2", name: "Italo", username: "italo", password: "calvino"},
-      %Rumbl.User{id: "3", name: "Luigi", username: "luigi", password: "pirandello"}
-    ]
-  end
-  def all(_module), do: []
-
-  def get(module, id) do
-    Enum.find all(module), fn map ->
-      map.id == id
-    end
-  end
-
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end
-  end
+  use Ecto.Repo, otp_app: :rumbl
 end
