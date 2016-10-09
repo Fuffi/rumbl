1b21e3fcadc44fff7cdd0ca52d6a1e9dfcf7fd38
Federico <ufinii@gmail.com>
2016-10-09 16:40:36 +0000

    Auth integration test

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,5 +1,20 @@
-# defmodule Rumbl.VideoControllerTest do
-#   use Rumbl.ConnCase
+defmodule Rumbl.VideoControllerTest do
+  use Rumbl.ConnCase
+
+  test "requires user authentication on all actions", %{conn: conn} do
+    Enum.each([
+        get(conn, video_path(conn, :new)),
+        get(conn, video_path(conn, :index)),
+        get(conn, video_path(conn, :show, "123")),
+        get(conn, video_path(conn, :edit, "123")),
+        put(conn, video_path(conn, :update, "123", %{})),
+        post(conn, video_path(conn, :create, %{})),
+        delete(conn, video_path(conn, :delete, "123"))
+      ], fn conn ->
+      assert html_response(conn, 302)
+      assert conn.halted
+    end)
+  end
 #
 #   alias Rumbl.Video
 #   @valid_attrs %{description: "some content", title: "some content", url: "some content"}
@@ -63,4 +78,4 @@
 #     assert redirected_to(conn) == video_path(conn, :index)
 #     refute Repo.get(Video, video.id)
 #   end
-# end
+end
