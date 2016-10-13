2593bd384446d6b5f5e1cf3ca5391c1e20fc18df
Federico Rebora <federico.rebora@carwow.co.uk>
2016-10-13 17:20:38 +0000

    More video controller tests

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,5 +1,21 @@
 defmodule Rumbl.VideoControllerTest do
   use Rumbl.ConnCase
+  alias Rumbl.Video
+
+  @valid_attrs %{url: "http://youtu.be", title: "vid", description: "a vid"}
+  @invalid_attrs %{title: "invalid"}
+
+  defp video_count(query), do: Repo.one(from v in query, select: count(v.id))
+
+  setup %{conn: conn} = config do
+    if username = config[:login_as] do
+      user = insert_user(username: username)
+      conn = assign(build_conn(), :current_user, user)
+      {:ok, conn: conn, user: user}
+    else
+      :ok
+    end
+  end
 
   test "requires user authentication on all actions", %{conn: conn} do
     Enum.each([
@@ -16,16 +32,6 @@ defmodule Rumbl.VideoControllerTest do
     end)
   end
 
-  setup %{conn: conn} = config do
-    if username = config[:login_as] do
-      user = insert_user(username: username)
-      conn = assign(build_conn(), :current_user, user)
-      {:ok, conn: conn, user: user}
-    else
-      :ok
-    end
-  end
-
   @tag login_as: "max"
   test "lists all user's videos on index", %{conn: conn, user: user} do
     user_video = insert_video(user, title: "funny cats")
@@ -36,6 +42,23 @@ defmodule Rumbl.VideoControllerTest do
     assert String.contains?(conn.resp_body, user_video.title)
     refute String.contains?(conn.resp_body, other_video.title)
   end
+
+  @tag login_as: "max"
+  test "creates user video and redirects", %{conn: conn, user: user} do
+    conn = post conn, video_path(conn, :create), video: @valid_attrs
+    assert redirected_to(conn) == video_path(conn, :index)
+    assert Repo.get_by!(Video, @valid_attrs).user_id == user.id
+  end
+
+  @tag login_as: "max"
+  test "does not create video and renders errors when invalid", %{conn: conn} do
+    count_before = video_count(Video)
+    conn = post conn, video_path(conn, :create), video: @invalid_attrs
+    assert html_response(conn, 200) =~ "check the errors"
+    assert video_count(Video) == count_before
+  end
+
+
 #
 #   alias Rumbl.Video
 #   @valid_attrs %{description: "some content", title: "some content", url: "some content"}
